8bed5ded13e6af09d2458f31e717bb96ec1d9061
zhangyujuan123 <zyj15231108763@163.com>
2020-02-26 09:10:02 +0800

    check js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,14 +1,14 @@
 module.exports = function (grunt) {
     grunt.initConfig({
-      csslint: {
+      eslint: {
         options: {
-          csslintrc: '.csslintrc'
+          eslintrc: './.eslintrc.json'
         },
-        src: ['*.css']
+        target: ['*.js']
       }
     });
   
-    grunt.loadNpmTasks('grunt-contrib-csslint');
+    grunt.loadNpmTasks('grunt-eslint');
   
-    grunt.registerTask('default', ['csslint']);
+    grunt.registerTask('default', ['eslint']);
   };
