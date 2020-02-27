8917cc4e632ad3038c8caabebe3f7cd6e15fb7e3
zhangyujuan123 <zyj15231108763@163.com>
2020-02-27 17:45:57 +0800

    test

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,14 +1,34 @@
 module.exports = function (grunt) {
-    grunt.initConfig({
-      eslint: {
-        options: {
-          eslintrc: './.eslintrc.json'
-        },
-        target: ['*.js']
+  grunt.initConfig({
+    mochacli: {
+      options: {
+        reporter: 'spec',
+        bail: true
+      },
+      all: ['test/*.js']
+    },
+    mocha_istanbul: {
+      coverage: {
+        src: 'test'
       }
-    });
-  
-    grunt.loadNpmTasks('grunt-eslint');
-  
-    grunt.registerTask('default', ['eslint']);
-  };
+    },
+    istanbul_check_coverage: {
+      default: {
+        options: {
+          coverageFolder: 'coverage*',
+          check: {
+            lines: 90,
+            statements: 90
+          }
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-mocha-cli');
+  grunt.loadNpmTasks('grunt-mocha-istanbul');
+
+  grunt.registerTask('default', ['mochacli']);   
+  grunt.registerTask('cover', ['mocha_istanbul']);
+  grunt.registerTask('check-cover', ['istanbul_check_coverage']);
+};
