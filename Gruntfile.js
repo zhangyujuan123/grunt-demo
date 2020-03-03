53a972c7ad22c865566cb8b6507abf1eb63dab36
zhangyujuan123 <zyj15231108763@163.com>
2020-03-03 22:53:06 +0800

    add

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,10 +1,14 @@
 module.exports = function (grunt) {
   grunt.initConfig({
-    cssmin:{
-      'rectangle.min.css':'rectangle.css'
+    // cssmin:{
+    //   'rectangle.min.css':'rectangle.css'
+    // }
+    uglify:{
+      'rectangle.min.js':'rectangle.js'
     }
   });
-
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
-  grunt.registerTask('default', ['cssmin']);
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.registerTask('default', ['uglify']);
+  // grunt.loadNpmTasks('grunt-contrib-cssmin');
+  // grunt.registerTask('default', ['cssmin']);
 };
